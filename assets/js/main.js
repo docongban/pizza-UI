bcb17b0d36ba00adfa51eba69e2bca9f0059b02f
docongban <banto2172001@gmail.com>
2021-10-12 15:56:05 +0700

    slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,66 @@ deliverys.forEach((delivery,index) => {
         search.classList.add('block')
     }
 })
+
+
+//Slider
+const slider=document.querySelector('.slider')
+const sliderMain = document.querySelector('.slider-main')
+const sliderItems = document.querySelectorAll('.slider-item')
+const nextBtn = document.querySelector('.slider-next')
+const prevBtn = document.querySelector('.slider-prev')
+const sliderItemWidth = sliderItems[0].offsetWidth
+const numberOfSliderItems = sliderItems.length 
+
+var slideNumber = 0
+
+
+//Next Slider
+nextBtn.addEventListener('click',() => {
+    
+    // sliderItems.forEach((slide,index) => {
+    //     slide.classList.remove('active')
+    // })
+    slideNumber++;
+    
+    if(slideNumber > (numberOfSliderItems-1) ){
+        slideNumber=0;
+    }
+
+    // sliderItems[slideNumber].classList.add('active')
+
+    sliderMain.style.left = "-"+sliderItemWidth*slideNumber+"px"
+})
+
+//Prev Slider
+prevBtn.addEventListener('click',() => {
+    // sliderItems.forEach((slide,index) => {
+    //     slide.classList.remove('active')
+    // })
+    slideNumber--;
+    
+    if(slideNumber < 0){
+        slideNumber = numberOfSliderItems-1;
+    }
+
+    // sliderItems[slideNumber].classList.add('active')
+
+    sliderMain.style.left = "-"+sliderItemWidth*slideNumber+"px"
+})
+
+//Auto Play
+
+var playSlider;
+var auto = () => {
+    playSlider = setInterval(() =>{
+        slideNumber++;
+        
+        if(slideNumber > (numberOfSliderItems-1) ){
+            slideNumber=0;
+        }
+    
+        sliderMain.style.left = "-"+sliderItemWidth*slideNumber+"px"
+    }, 4000)
+}
+
+auto()
